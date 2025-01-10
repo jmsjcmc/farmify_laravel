e9c3ca0d2a261566186c853cef8ae7b5603ae371
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-01-10 21:29:44 +0800

    Update: Use and show authenticated user name and email

--- resources/views/layouts/consumer.blade.php
+++ resources/views/layouts/consumer.blade.php
@@ -39,9 +39,9 @@
                     <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
                         id="user-dropdown">
                         <div class="px-4 py-3">
-                            <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
+                            <span class="block text-sm text-gray-900 dark:text-white">{{ Auth::user()->first_name}} {{ Auth::user()->last_name}}</span>
                             <span
-                                class="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
+                                class="block text-sm  text-gray-500 truncate dark:text-gray-400">{{Auth::user()->email}}</span>
                         </div>
                         <ul class="py-2" aria-labelledby="user-menu-button">
                             <li>
@@ -56,6 +56,7 @@
                                 <a href="#"
                                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
                             </li>
+
                             <li>
                                 <a href="#"
                                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
