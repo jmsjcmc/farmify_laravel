422cd33580457c5acdcf5b9943350351e9cd2c44
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-01-12 19:23:36 +0800

    Update: Added dark overlay to darken bg

--- resources/views/admin/user-management/user-add-edit.blade.php
+++ resources/views/admin/user-management/user-add-edit.blade.php
@@ -1,6 +1,7 @@
 <!-- Add/Edit User Modal -->
 <div id="userModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative w-full max-w-2xl max-h-full">
+   <div class="fixed inset-0 bg-black bg-opacity-50 transition-opacity"></div>
+    <div class="relative w-full max-w-2xl max-h-full mx-auto mt-10">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <!-- Modal header -->
