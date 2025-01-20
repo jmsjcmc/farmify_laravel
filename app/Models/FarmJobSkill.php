0b8b93937f50469810599ad3ea14e62497eb5ad9
jmsjcmc <jecemecojamestabilog@gmail.com>
2025-01-20 19:58:20 +0800

    Add relationships in farm job skill model

--- app/Models/FarmJobSkill.php
+++ app/Models/FarmJobSkill.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FarmJobSkill extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'farm_job_id',
+        'skill_name',
+        'skill_level'
+    ];
+
+    public function job()
+    {
+        return $this->belongsTo()
+    }
 }
